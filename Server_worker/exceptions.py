c667bc73f50777bab8004587b06e97145b1318a9
RUmskLev <lev.lobanob.g@gmail.com>
2023-12-22 15:38:50 +0300

    Changed updates logic, added more info to db

--- Server_worker/exceptions.py
+++ Server_worker/exceptions.py
@@ -1,22 +1,53 @@
 class DatabaseException(Exception):
-    pass
+    def __init__(self, message="A database error occurred."):
+        self.message = message
+        super().__init__(self.message)
 
 
 class NoData(DatabaseException):
-    pass
+    def __init__(self, message="No data found."):
+        super().__init__(message)
 
 
 class NoUser(DatabaseException):
-    pass
+    def __init__(self, message="User not found."):
+        super().__init__(message)
 
 
 class NoRecipient(NoUser):
-    pass
+    def __init__(self, message="Recipient not found."):
+        super().__init__(message)
 
 
 class UsernameTaken(DatabaseException):
-    pass
+    def __init__(self, message="Username is already taken."):
+        super().__init__(message)
 
 
 class NoMessages(DatabaseException):
-    pass
+    def __init__(self, message="No messages found."):
+        super().__init__(message)
+
+
+class UserNotFoundError(NoUser):
+    def __init__(self, username):
+        message = f"No user with username {username} found."
+        super().__init__(message)
+
+
+class MessageInsertionError(DatabaseException):
+    def __init__(self):
+        message = "An error occurred while inserting the message."
+        super().__init__(message)
+
+
+class UserUpdateError(DatabaseException):
+    def __init__(self):
+        message = "An error occurred while updating the user."
+        super().__init__(message)
+
+
+class HashMismatchError(Exception):
+    def __init__(self):
+        self.message = "The provided hash does not match the hash of the message."
+        super().__init__(self.message)
